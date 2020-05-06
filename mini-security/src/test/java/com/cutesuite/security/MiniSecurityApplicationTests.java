ef6a56a3e77dcb8427aab614ad0cc3a843db9ae4
kevin zhang <workeast@163.com>
2020-05-06 14:17:50 +0800

    jdbc,jpa part

---
+++ mini-security/src/test/java/com/cutesuite/security/MiniSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cutesuite.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
